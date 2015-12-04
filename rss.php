f35ffb4ee6e58b4f2059f2555afa03454908a8bb
Reccy <aaronmeaney@hotmail.com>
2015-12-04 21:51:04 +0000

    AM - Added host checker for RSS feeder string

--- rss.php
+++ rss.php
@@ -10,7 +10,7 @@
         $proc->importStyleSheet($xsl);
         
         echo $proc->transformToXML($xml);
-        echo "<p class='col-sm-12' id='intro-subtext'>Link for RSS Reader: https://rps-wadproject-reccy-1.c9users.io/rss.xml</p>";
+        echo "<p class='col-sm-12' id='intro-subtext'>Link for RSS Reader: https://".$_SERVER['HTTP_HOST']."/rss.xml";
         exit();
     } else {
         echo "<p id='intro-text'>This is where the RSS feed should be.</p>
